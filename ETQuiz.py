46917d300b21b672a4c38008bce291cec2ae918c
dadiletta <dan.adiletta@gmail.com>
2015-10-20 08:57:14 -0400

    Added in a turtle

---
+++ ETQuiz.py
@@ -0,0 +1,4 @@
+somenumber = 5
+somestring = "A string is an array of characters"
+for x in range(somenumber):
+    print somestring
---
+++ HelloWorldB.py
@@ -0,0 +1,9 @@
+import turtle
+screen = turtle.Screen()
+screen.title('Square Demo')
+t = turtle.Turtle()
+for i in range(400):
+    t.goto(i,i)
+    if i % 20 == 0:
+        t.write(str(i), font=("Arial",12,"normal"))
+d = input()
